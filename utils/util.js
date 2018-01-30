25a3f9e1d33ce020164dba4f9e9856f950e3ce46
ljc <liujingchaoljc@163.com>
2018-01-30 10:49:37 +0800

    修改部分demo

--- utils/util.js
+++ utils/util.js
@@ -43,10 +43,65 @@ const getRandomArrayElement = arr => {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+// 加载配置文件
+const config = require('../global_config.js');
+// 格式化时间戳
+function getTime(timestamp) {
+  var time = arguments[0] || 0;
+  var t, y, m, d, h, i, s;
+  t = time ? new Date(time * 1000) : new Date();
+  y = t.getFullYear();    // 年
+  m = t.getMonth() + 1;   // 月
+  d = t.getDate();        // 日
+
+  h = t.getHours();       // 时
+  i = t.getMinutes();     // 分
+  s = t.getSeconds();     // 秒
+
+  return [y, m, d].map(formatNumber).join('-') + ' ' + [h, i, s].map(formatNumber).join(':');
+
+}
+
 
 module.exports = {
   formatTime: formatTime,
   getRandomArray: getRandomArray,
   getRandomArrayElement: getRandomArrayElement,
+
+  getTime: getTime,
+  //请求数据
+  AJAX: function (data = '', fn, method = "get", header = {}) {
+    wx.request({
+      url: config.API_HOST + data,
+      method: method ? method : 'get',
+      data: {},
+      header: header ? header : { "Content-Type": "application/json" },
+      success: function (res) {
+        fn(res);
+      }
+    });
+  },
+
+  /**
+ * 获取格式化日期
+ * 20161002
+ */
+  getFormatDate: function (str) {
+
+    // 拆分日期为年 月 日
+    var YEAR = str.substring(0, 4),
+      MONTH = str.substring(4, 6),
+      DATE = str.slice(-2);
+
+    // 拼接为 2016/10/02 可用于请求日期格式
+    var dateDay = YEAR + "/" + MONTH + "/" + DATE;
+
+    // 获取星期几
+    var week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+      day = new Date(dateDay).getDay();
+    return {
+      "dateDay": MONTH + "月" + DATE + "日 " + week[day]
+    }
+  },
 }
 
